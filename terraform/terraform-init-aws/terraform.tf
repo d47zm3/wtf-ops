5f8b4df0aa687e4c10dc09732eada6101e499ae5
Damian Tykalowski <damian@d47zm3.me>
2022-05-13 13:51:50 +0200

    quick fix

--- terraform/terraform-init-aws/terraform.tf
+++ terraform/terraform-init-aws/terraform.tf
@@ -8,15 +8,16 @@ terraform {
   backend "s3" {
     bucket         = "<BUCKET_NAME>"
     key            = "state/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-west-1"
     encrypt        = true
     kms_key_id     = "alias/terraform-bucket-key"
     dynamodb_table = "terraform-state"
+    profile = "<MAKE_SURE_I_EXIST>"
   }
   required_version = ">= 1.1"
 }
 
 provider "aws" {
-  profile = "MAKE_SURE_I_EXIST"
+  profile = "<MAKE_SURE_I_EXIST>"
   region  = "eu-west-1"
 }
